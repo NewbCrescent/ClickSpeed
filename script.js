eab65edf2d7388f6edfe43ae041bb140aac29551
NewbCrescent <urielmarinez26@gmail.com>
2023-12-27 08:59:36 -0600

    Mobile and grammar improvements

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const cpsButton = document.getElementById("clicked");
 const clickContainer = document.getElementById("clicker-container");
 const timer = document.querySelector("div p");
 const headTimer = document.querySelector("div h2 span");
+const plurals = document.querySelectorAll(".plural");
 const restartButton = document.getElementById("restart");
 const secondsTextElement = document.getElementById("seconds");
 const cpsTextElement = document.querySelector("#cps");
@@ -18,13 +19,28 @@ function restartActivated() {
     headTimer.textContent = totalSeconds;
     secondsTextElement.textContent = totalSeconds;
     cpsTextElement.textContent = "Haven't Started";
+    if (headTimer.textContent == "1") {
+        pluralWords("");
+    }
     clearInterval(interval);
+}
+
+function pluralWords(char) {
+    for (let i = 0; i < plurals.length; ++i) {
+        plurals[i].textContent = char;
+    }
 }
 
 for (let i = 0; i < allChoosingButtons.length; ++i) {
     allChoosingButtons[i].onclick = function () {
         const secondsFromButton = +allChoosingButtons[i].firstChild.textContent; //first child is the span element
         totalSeconds = secondsFromButton;
+        if (secondsFromButton == 1) {
+            pluralWords("");
+        }
+        else if (plurals[0].textContent == "") {
+            pluralWords("s")
+        }
         restartActivated();
         restartButton.style.display = "none";
     };
@@ -33,12 +49,18 @@ for (let i = 0; i < allChoosingButtons.length; ++i) {
 function timerDecreases() {
     if (seconds > 0) {
         --seconds;
+        if (seconds == 1) {
+            plurals[1].textContent = ""
+        }
+        else if (seconds == 0) {
+            plurals[1].textContent = "s"
+        }
         secondsTextElement.textContent = seconds;
     }
 }
 
 function resultClicks() {
-    cpsTextElement.textContent = click/totalSeconds;
+    cpsTextElement.textContent = Math.round(click/totalSeconds * 100) / 100; //rounding to two decimal places
 }
 
 function trackClicks() {
